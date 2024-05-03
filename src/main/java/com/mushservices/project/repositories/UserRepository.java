7db3ef875bec5bc4affea7edc97bf8fd836a3f31
Ramon Chaves <144055828+Mushytu@users.noreply.github.com>
2024-05-03 14:39:07 -0300

    JPA repository, dependency injection, database seeding

---
+++ src/main/java/com/mushservices/project/repositories/UserRepository.java
@@ -0,0 +1,9 @@
+package com.mushservices.project.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.mushservices.project.entities.User;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+	
+}
