a8d6092740ddfc1c330ae1d732c86bad8d808044
Ramon Chaves <144055828+Mushytu@users.noreply.github.com>
2024-05-17 15:12:36 -0300

    OrderItemPK, OrderItem

--- src/main/java/com/mushservices/project/config/TestConfig.java
+++ src/main/java/com/mushservices/project/config/TestConfig.java
@@ -10,6 +10,7 @@ import org.springframework.context.annotation.Profile;
 
 import com.mushservices.project.entities.Category;
 import com.mushservices.project.entities.Order;
+import com.mushservices.project.entities.OrderItem;
 import com.mushservices.project.entities.Product;
 import com.mushservices.project.entities.User;
 import com.mushservices.project.entities.enums.OrderStatus;
@@ -69,6 +70,13 @@ public class TestConfig implements CommandLineRunner {
 		p5.getCategories().add(cat2);
 		
 		prodRep.saveAll(Arrays.asList(p1, p2, p3, p4, p5));
+		
+		OrderItem oi1 = new OrderItem(o1, p1, 2, p1.getPrice());
+		OrderItem oi2 = new OrderItem(o1, p3, 1, p3.getPrice());
+		OrderItem oi3 = new OrderItem(o2, p3, 2, p3.getPrice());
+		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice()); 
+		
+		s
 		
 		
 	}
