522a3735c6441675b4675a8d22b2a8030d619d70
Ramon Chaves <144055828+Mushytu@users.noreply.github.com>
2024-06-03 15:05:27 -0300

    Exception handling - findById

---
+++ src/main/java/com/mushservices/project/resources/exceptions/ResourceExceptionHandler.java
@@ -0,0 +1,24 @@
+package com.mushservices.project.resources.exceptions;
+
+import java.time.Instant;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import com.mushservices.project.services.exceptions.ResourceNotFoundException;
+
+import jakarta.servlet.http.HttpServletRequest;
+
+@ControllerAdvice
+public class ResourceExceptionHandler {
+
+	@ExceptionHandler(ResourceNotFoundException.class)
+	public ResponseEntity<StandardError> resourceNotFound(ResourceNotFoundException e1, HttpServletRequest request) {
+		String errorMessage = "Resource not found";
+		HttpStatus status = HttpStatus.NOT_FOUND; 
+		StandardError error = new StandardError(Instant.now(), status.value(), errorMessage, e1.getMessage(), request.getRequestURI());
+		return ResponseEntity.status(status).body(error);
+	}
+}
